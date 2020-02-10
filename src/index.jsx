5a107887d5ea142359319c1721a6a88a58d960b8
Maciej Rim <maciej.rim@gmail.com>
2020-02-10 16:34:07 +0100

    added react router to index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'react-jss';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createHashHistory } from 'history';
 
+// JSS theme
 const theme = {};
 
+// Hash history needed for the router
+const hashHistory = createHashHistory();
+
+// Simple loading component
+const Loading = () => <div>Loading...</div>;
+
+// Main component
 const Root = () => {
-	return <ThemeProvider theme={theme}>React template</ThemeProvider>;
+	return (
+		// ThemeProvider provides theme for all components
+		<ThemeProvider theme={theme}>
+			{/* Router declaration */}
+			<Router history={hashHistory}>
+				{/* Suspense needed for route switching */}
+				<React.Suspense fallback={<Loading />}>
+					{/* Switch ensures only single route is rendered */}
+					<Switch>
+						{/* Root route declaration */}
+						<Route path="/" exact>
+							React template
+						</Route>
+					</Switch>
+				</React.Suspense>
+			</Router>
+		</ThemeProvider>
+	);
 };
 
 // Render App with providers on #root
